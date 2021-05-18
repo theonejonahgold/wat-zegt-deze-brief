bf70051a89388cc02ee3e3968af80cf96039ffde
bvictorb <victory-boucher@hotmail.com>
2021-05-18 17:44:54 +0200

    feat(fe): add alert and pagination components

---
+++ front-end/src/lib/components/molecules/Alert.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Button from '$atoms/Button.svelte'
+	import Image from '$atoms/Image.svelte'
+	import SpokenText from '$atoms/SpokenText.svelte'
+	import Help from '$atoms/Help.svelte'
+
+	export let audioFile: HTMLAudioElement
+	export let src: string
+	export let alt: string
+</script>
+
+<div>
+	<Help />
+	<SpokenText {audioFile} />
+	<Image {src} {alt} />
+	<Button on:click>Oke</Button>
+</div>
