bcbc2b40e8cf27baa236c9ea5cf1278ed14577bb
bvictorb <victory-boucher@hotmail.com>
2021-05-18 10:49:25 +0200

    feat(fe): add field and input components

--- front-end/src/lib/components/atoms/Input.svelte
+++ front-end/src/lib/components/atoms/Input.svelte
@@ -1,6 +1,27 @@
 <script>
-	export let placeholder: string
-	export let type: string = 'text'
+	import { createEventDispatcher } from 'svelte'
+
+	type inputTypes = 'text' | 'email' | 'number' | 'tel' | 'range'
+	type inputEvent = Event & { currentTarget: EventTarget & HTMLInputElement }
+
+	export let value = ''
+	export let type: inputTypes = 'text'
+	export let name = ''
+
+	const dispatch =
+		createEventDispatcher<{
+			valueChange: {
+				name: string
+				value: string
+			}
+		}>()
+
+	const inputHandler = (e: inputEvent) => {
+		dispatch('valueChange', {
+			name,
+			value: e.currentTarget.value,
+		})
+	}
 </script>
 
-<input on:change {placeholder} {type} />
+<input on:input={inputHandler} {value} {name} {type} {...$$restProps} />
