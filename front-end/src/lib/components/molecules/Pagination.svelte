c59669151e4ca9be6d726494d301144ec9b036b6
bvictorb <victory-boucher@hotmail.com>
2021-05-19 15:52:56 +0200

    feat(fe): add animation to pagination

--- front-end/src/lib/components/molecules/Pagination.svelte
+++ front-end/src/lib/components/molecules/Pagination.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { flip } from 'svelte/animate'
+	import { quadInOut } from 'svelte/easing'
+
 	export let selected: number
 	export let amount: number
 	export let items: Array<any>
@@ -6,8 +9,30 @@
 	const steps = Array(items ? items.length : amount)
 </script>
 
+<style lang="scss">
+	div {
+		width: 100%;
+		height: 1em;
+		display: flex;
+		background: rgb(123, 123, 255);
+
+		span {
+			display: inline-block;
+			height: 1em;
+			width: 100%;
+
+			&.active {
+				background: blue;
+			}
+		}
+	}
+</style>
+
 <div>
-	{#each steps as _, i}
-		<span class={i === selected ? 'active' : ''} />
+	{#each steps as _, i (i + 1 === selected ? 'active' : i)}
+		<span
+			class={i + 1 === selected ? 'active' : ''}
+			animate:flip={{ duration: 500, easing: quadInOut }}
+		/>
 	{/each}
 </div>
