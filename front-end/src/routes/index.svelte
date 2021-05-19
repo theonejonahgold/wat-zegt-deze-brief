305890c33681c231621389c955f93fbc61fe32e7
benl95 <beno_1995@hotmail.com>
2021-05-19 16:30:46 +0200

    feat(fe): add index layout

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -1,10 +1,24 @@
 <script>
-	import { Icon, NavItem, Title } from '$atoms'
+	import { Title, Icon, Button, SpokenText, Section } from '$atoms'
+	import { IndexIcon, Oval } from '$icons'
+
+	let content = [
+		{ text: 'Hallo!' },
+		{ text: 'Dit is de app die helpt met het uitleggen van brieven' },
+		{ text: 'Start' },
+	]
+
+	// let audioFile: HTMLAudioElement
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<h1>hello world!</h1>
-<Title>Hoi</Title>
+<Oval />
+<Section>
+	<Title>{content[0].text}</Title>
+	<!-- <SpokenText {audioFile}>{content[1].text}</SpokenText> -->
+	<Icon><IndexIcon /></Icon>
+	<Button>{content[2].text}</Button>
+</Section>
