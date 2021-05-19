01a54b248c1f3b45b245822ef1750bb61b91fb3d
benl95 <beno_1995@hotmail.com>
2021-05-19 16:28:58 +0200

    feat(fe): style title component

--- front-end/src/lib/components/atoms/Title.svelte
+++ front-end/src/lib/components/atoms/Title.svelte
@@ -1,1 +1,18 @@
+<style>
+	:global(:root) {
+		--font: 'Avenir Black';
+		--title-size: 2.5rem;
+		--letter-spacing: 0.3px;
+		--title-color: #434343;
+	}
+
+	h1 {
+		margin: 0.625rem 2.125rem 1rem 2.125rem;
+		font-family: var(--font);
+		font-size: var(--title-size);
+		letter-spacing: var(--letter-spacing);
+		color: var(--title-color);
+	}
+</style>
+
 <h1><slot /></h1>
