12a590c2abc2afb1bab7eeb36ef47e2a7e83353f
benl95 <beno_1995@hotmail.com>
2021-05-19 16:27:57 +0200

    feat(fe): style button component

--- front-end/src/lib/components/atoms/Button.svelte
+++ front-end/src/lib/components/atoms/Button.svelte
@@ -2,6 +2,36 @@
 	export let type: 'button' | 'anchor' = 'button'
 </script>
 
+<style>
+	:global(:root) {
+		--font-button: 'Avenir medium';
+		--button-size: 1.125rem;
+		--text-color: #ffffff;
+		--button-background: linear-gradient(to right, #80b6fe, #978fff);
+		--button-shadow: drop-shadow(0px 3px 8px #0000002a);
+		--button-border-radius: 30px;
+		--letter-spacing: 0.3px;
+		--button-width: 18.375rem;
+		--button-height: 3.313rem;
+	}
+
+	button {
+		display: block;
+		cursor: pointer;
+		color: var(--text-color);
+		font-size: var(--button-size);
+		margin: auto;
+		padding: 0;
+		background: var(--button-background);
+		filter: var(--button-shadow);
+		border: none;
+		border-radius: var(--button-border-radius);
+		letter-spacing: var(--letter-spacing);
+		width: var(--button-width);
+		height: var(--button-height);
+	}
+</style>
+
 {#if type === 'button'}
 	<button on:click><slot /></button>
 {:else}
