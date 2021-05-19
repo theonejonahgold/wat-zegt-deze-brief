94fb169cccdc137f36dbbcf699f1677079318933
Jonah Meijers <jonahmeijers97@gmail.com>
2021-05-19 12:05:53 +0200

    build(fe): add ts paths to svelte config

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -1,5 +1,10 @@
+<script>
+	import { Icon, NavItem, Title } from '$atoms'
+</script>
+
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <h1>hello world!</h1>
+<Title>Hoi</Title>
--- front-end/svelte.config.js
+++ front-end/svelte.config.js
@@ -1,8 +1,12 @@
+import { resolve } from 'path'
 import node from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 import autoprefixer from 'autoprefixer'
 import vhFix from 'postcss-100vh-fix'
 import cssnano from 'cssnano'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const tsConfig = require('./tsconfig.json')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,6 +22,18 @@ const config = {
 	kit: {
 		target: '#svelte',
 		adapter: node(),
+		vite: config => ({
+			...config,
+			resolve: {
+				alias: Object.entries(tsConfig.compilerOptions.paths).reduce(
+					(acc, [alias, path]) => ({
+						...acc,
+						[alias]: resolve(import.meta.url, `/${path}`),
+					}),
+					{}
+				),
+			},
+		}),
 	},
 }
 
