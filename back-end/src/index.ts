f3aa9db975553ddaa6861d3b94e199da4285c49c
Jonah Meijers <jonahmeijers97@gmail.com>
2021-05-19 15:33:40 +0200

    feat(be): add register route

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { json, urlencoded } from 'express'
+import router from './router/index.js'
 
 main()
 
@@ -6,7 +7,9 @@ async function main() {
 	const app = express()
 	const port = process.env.PORT || 5000
 
-	app.get('/', (_, res) => res.send('hoi!'))
+	app.use(urlencoded({ extended: true }))
+	app.use(json())
+	app.use(router)
 
 	app.listen(port, () => console.log(`Listening on port ${port}`))
 }
---
+++ back-end/src/router/user.ts
@@ -0,0 +1,29 @@
+import { definitions } from '$types/supabase'
+import { Router } from 'express'
+import { getSupabaseClient } from '../supabase/index.js'
+
+const client = getSupabaseClient()
+
+const userRouter = Router().post('/register', (req, res) => {
+	const { email, password, role } = req.body
+	if (!role) return res.status(400).send('No user role was provided.')
+	client.auth
+		.signUp({
+			email,
+			password,
+		})
+		.then(({ user, error }) => {
+			if (error) throw error
+			client
+				.from<definitions['users']>('users')
+				.update({ role })
+				.eq('id', user!.id)
+				.then(({ data }) => {
+					if (error) throw error
+					res.status(200).send({ data })
+				})
+		})
+		.catch(error => res.status(400).send(error.message))
+})
+
+export { userRouter }
