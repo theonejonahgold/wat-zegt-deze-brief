a52507ad9d6f9bc75ab1b631d3cf724c10a7b396
bvictorb <victory-boucher@hotmail.com>
2021-05-19 15:59:04 +0200

    feat(fe): add button click handling to alert component

--- front-end/src/lib/components/molecules/Alert.svelte
+++ front-end/src/lib/components/molecules/Alert.svelte
@@ -1,9 +1,19 @@
 <script>
+	import { createEventDispatcher } from 'svelte'
 	import { Button, Image, SpokenText, Help } from '$atoms'
 
 	export let audioFile: HTMLAudioElement
 	export let src: string
 	export let alt: string
+
+	const dispatch =
+		createEventDispatcher<{
+			close: null | undefined
+		}>()
+
+	function buttonClickHandler() {
+		dispatch('close')
+	}
 </script>
 
 <!-- TODO: how to properly handle multiple on:clicks? -->
@@ -11,5 +21,5 @@
 	<Help on:click />
 	<SpokenText {audioFile} />
 	<Image {src} {alt} />
-	<Button on:click>Oke</Button>
+	<Button on:click={buttonClickHandler}>Oke</Button>
 </div>
