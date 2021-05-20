2a1a4b750edb2befd7dbbb8ddde1d58ba6cc746c
benl95 <beno_1995@hotmail.com>
2021-05-20 14:38:54 +0200

    feat(fe): add AnchorIcon to Back component

--- front-end/src/lib/components/atoms/Back.svelte
+++ front-end/src/lib/components/atoms/Back.svelte
@@ -1,5 +1,16 @@
 <script>
+	import { AnchorIcon } from '$icons'
+
 	export let href: string
 </script>
 
-<a {href}>&lt;</a>
+<style>
+	a {
+		text-decoration: none;
+		color: #9593fc;
+		font-weight: 900;
+		font-size: 20px;
+	}
+</style>
+
+<a {href}><AnchorIcon /></a>
