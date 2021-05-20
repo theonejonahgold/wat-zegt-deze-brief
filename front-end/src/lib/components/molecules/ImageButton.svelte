40e259bc708a299d9fd1df1f14243c4f38c4de9d
benl95 <beno_1995@hotmail.com>
2021-05-20 14:51:37 +0200

    feat(fe): make ImageButton dynamic and add styling

--- front-end/src/lib/components/molecules/ImageButton.svelte
+++ front-end/src/lib/components/molecules/ImageButton.svelte
@@ -1,14 +1,44 @@
 <script>
-	import { Button, Image, SpokenText } from '$atoms'
+	import { Image, Input } from '$atoms'
 
 	export let src: string
 	export let alt: string
-	export let audioFile: HTMLAudioElement
+	export let value: string
+	export let name: string
+	export let href: string = ''
+	export let id: string
 </script>
 
-<Button on:click>
-	<Image {src} {alt} />
-</Button>
-<SpokenText {audioFile}>
-	<slot />
-</SpokenText>
+<style>
+	:global(input[type='radio']) {
+		margin: 0;
+		padding: 0;
+		appearance: none;
+	}
+
+	:global(input[type='radio']:checked + label) {
+		box-shadow: 0 0 0 3px #9593fc;
+	}
+
+	:global(label) {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #ffffff;
+		box-shadow: 0px 3px 4px #cecece;
+		height: 10rem;
+		width: 17.375rem;
+		border-radius: 10px;
+	}
+</style>
+
+{#if href}
+	<a {href}>
+		<Image {src} {alt} />
+	</a>
+{:else}
+	<Input {value} {name} {id} type="radio" />
+	<label for={id}>
+		<Image {src} {alt} />
+	</label>
+{/if}
