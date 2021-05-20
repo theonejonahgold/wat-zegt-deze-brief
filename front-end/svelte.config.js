c9b5ee93c93f6ee7903dbf8b8b76302ae2270641
Jonah Meijers <jonahmeijers97@gmail.com>
2021-05-20 11:45:29 +0200

    build(fe): set vite mode based on node env

--- front-end/svelte.config.js
+++ front-end/svelte.config.js
@@ -25,6 +25,7 @@ const config = {
 		adapter: node(),
 		vite: config => ({
 			...config,
+			mode: process.env.NODE_ENV || 'development',
 			resolve: {
 				alias: Object.entries(tsConfig.compilerOptions.paths).reduce(
 					(acc, [alias, path]) => ({
