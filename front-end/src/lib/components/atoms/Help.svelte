8e24bb1e93023405fce6bf68a4fbadb7c6a76683
benl95 <beno_1995@hotmail.com>
2021-05-20 14:39:27 +0200

    feat(fe): style Help component

--- front-end/src/lib/components/atoms/Help.svelte
+++ front-end/src/lib/components/atoms/Help.svelte
@@ -1,1 +1,15 @@
+<style>
+	button {
+		background: none;
+		border: 3px solid #9593fc;
+		border-radius: 50%;
+		width: 2rem;
+		height: 2rem;
+		color: #9593fc;
+		font-weight: 800;
+		font-size: 20px;
+		cursor: pointer;
+	}
+</style>
+
 <button on:click aria-label="Leg dit scherm uit">?</button>
