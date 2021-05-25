25e9a16ec5ffbd22aa7febeeee4639d5ee5dd52d
benl95 <beno_1995@hotmail.com>
2021-05-25 15:57:15 +0200

    feat(fe): add form for letter

---
+++ src/routes/dashboard/letter/form.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Form } from '$organisms'
+</script>
+
+<main>
+	<h2>Stuur brief in</h2>
+	<Form
+		noEnhance
+		action="/api/letter"
+		fields={[
+			{
+				label: 'Foto brief',
+				name: 'page',
+				type: 'file',
+				autofocus: true,
+				placeholder: 'Nuon',
+				required: true,
+			},
+		]}
+		method="POST"
+	>
+		<svelte:fragment slot="submit">Stuur foto</svelte:fragment>
+	</Form>
+</main>
