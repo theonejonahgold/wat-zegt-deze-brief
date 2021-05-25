b97877f9376e2c08808128fdff93a0d4421f2225
Jonah Meijers <jonahmeijers97@gmail.com>
2021-05-25 10:01:52 +0200

    refactor: move role check to register-only handler

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -9,14 +9,15 @@ export const post: RequestHandler<Locals, FormData> = async ({ body }) => {
 	const name = body.get('name')
 	const authType = body.get('auth-type') as 'register' | 'login'
 
-	if (!role)
-		return {
-			status: 400,
-			body: 'Please provide a user role',
-		}
-
 	try {
-		if (authType === 'register') await register({ email, password, role, name })
+		if (authType === 'register') {
+			if (!role)
+				return {
+					status: 400,
+					body: 'Please provide a user role',
+				}
+			await register({ email, password, role, name })
+		}
 	} catch (err) {
 		return {
 			status: 400,
