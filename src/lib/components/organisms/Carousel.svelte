e4713f950032d7cace32995f66ce30dfa7f60b46
benl95 <beno_1995@hotmail.com>
2021-05-26 15:32:28 +0200

    refactor(fe): create Carousel component for carousel

---
+++ src/lib/components/organisms/Carousel.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { Icon } from '$atoms'
+	import { Image } from '$atoms'
+	import { getContext } from 'svelte'
+	import type { Writable } from 'svelte/store'
+
+	const pageStore = getContext<Writable<any>>('previews')
+
+	let index: number = 0
+
+	const next = () => {
+		index = (index + 1) % $pageStore.length
+	}
+</script>
+
+{#if $pageStore}
+	<section>
+		<button on:click={next} id="previous"><Icon>&gt;</Icon></button>
+		<div>
+			{#each [$pageStore[index]] as page (index)}
+				<Image src={page} alt="Page preview" />
+			{/each}
+		</div>
+		<button on:click={next} id="previous"><Icon>&lt;</Icon></button>
+	</section>
+{/if}
