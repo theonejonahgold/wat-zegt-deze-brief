dfa3289f51e52f88951fb31216fce803d59b7ab5
benl95 <beno_1995@hotmail.com>
2021-05-26 15:32:28 +0200

    refactor(fe): create Carousel component for carousel

--- src/lib/components/organisms/Carousel.svelte
+++ src/lib/components/organisms/Carousel.svelte
@@ -1,62 +1,26 @@
 <script>
 	import { Icon } from '$atoms'
 	import { Image } from '$atoms'
-	import { AnchorIcon } from '$icons'
+	import { getContext } from 'svelte'
+	import type { Writable } from 'svelte/store'
 
-	export let pages: string[]
-	export let selected: number = 0
+	const pageStore = getContext<Writable<any>>('previews')
+
+	let index: number = 0
 
 	const next = () => {
-		selected = (selected + 1) % pages.length
+		index = (index + 1) % $pageStore.length
 	}
 </script>
 
-<style>
-	section {
-		display: flex;
-		align-items: center;
-		height: 100%;
-		position: relative;
-	}
-
-	button {
-		border: none;
-		background: none;
-		opacity: 0.8;
-		padding: var(--space-s);
-		width: 3rem;
-		height: 3rem;
-		cursor: pointer;
-		position: absolute;
-		z-index: 1;
-	}
-
-	#next {
-		transform: rotate(180deg);
-		right: var(--space-s);
-	}
-
-	#previous {
-		left: var(--space-s);
-	}
-
-	button:hover {
-		background-color: var(--grey);
-	}
-
-	div {
-		position: relative;
-		z-index: -1;
-		margin: var(--space-s);
-	}
-</style>
-
-<section>
-	<button on:click={next} id="previous"><Icon><AnchorIcon /></Icon></button>
-	<div>
-		{#each [pages[selected]] as page (selected)}
-			<Image src={page} alt="Page preview" shadow={true} />
-		{/each}
-	</div>
-	<button on:click={next} id="next"><Icon><AnchorIcon /></Icon></button>
-</section>
+{#if $pageStore}
+	<section>
+		<button on:click={next} id="previous"><Icon>&gt;</Icon></button>
+		<div>
+			{#each [$pageStore[index]] as page (index)}
+				<Image src={page} alt="Page preview" />
+			{/each}
+		</div>
+		<button on:click={next} id="previous"><Icon>&lt;</Icon></button>
+	</section>
+{/if}
