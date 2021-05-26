d9b00b5185084f514d867e4bc80e5ad369fcf2cb
benl95 <beno_1995@hotmail.com>
2021-05-26 15:32:28 +0200

    refactor(fe): create Carousel component for carousel

--- src/lib/components/organisms/index.ts
+++ src/lib/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export { default as Form } from './Form.svelte'
 export { default as LetterCard } from './LetterCard.svelte'
 export { default as Nav } from './Nav.svelte'
+export { default as Carousel } from './Carousel.svelte'
