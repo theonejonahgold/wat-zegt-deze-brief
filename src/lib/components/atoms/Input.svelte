a8df330bdd1299b99ac02f058db7dc6f9020084c
Jonah Meijers <jonahmeijers97@gmail.com>
2021-05-26 15:50:27 +0200

    refactor: simplify input component

--- src/lib/components/atoms/Input.svelte
+++ src/lib/components/atoms/Input.svelte
@@ -1,23 +1,9 @@
 <script>
 	import type { InputType } from '$types'
-	import { getContext, setContext } from 'svelte'
-	import type { Writable } from 'svelte/store'
 
 	export let value = ''
 	export let type: InputType = 'text'
 	export let name = ''
-
-	const pageStore = getContext<Writable<string | ArrayBuffer>>('previews')
-
-	const onFileSelected = e => {
-		let image = e.target.files[0]
-		let reader = new FileReader()
-		reader.readAsDataURL(image)
-		reader.onload = e => {
-			let page = e.target.result
-			pageStore.set(page)
-		}
-	}
 </script>
 
 <style>
@@ -30,8 +16,4 @@
 	}
 </style>
 
-{#if type === 'file'}
-	<input on:change={e => onFileSelected(e)} {value} {name} {type} {...$$restProps} />
-{:else}
-	<input {value} {name} {type} {...$$restProps} />
-{/if}
+<input on:change {value} {name} {type} {...$$restProps} />
