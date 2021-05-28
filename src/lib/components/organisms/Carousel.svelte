3bb26f81f5811d68d17602c56104c0b3d2d5f09b
benl95 <beno_1995@hotmail.com>
2021-05-28 09:06:07 +0200

    feat: add carousel, make responsive with pagelist

--- src/lib/components/organisms/Carousel.svelte
+++ src/lib/components/organisms/Carousel.svelte
@@ -1,26 +1,66 @@
 <script>
 	import { Icon } from '$atoms'
 	import { Image } from '$atoms'
-	import { getContext } from 'svelte'
-	import type { Writable } from 'svelte/store'
+	import { AnchorIcon } from '$icons'
 
-	const pageStore = getContext<Writable<any>>('previews')
-
-	let index: number = 0
+	export let pages: string[]
+	export let selected: number = 0
 
 	const next = () => {
-		index = (index + 1) % $pageStore.length
+		selected = (selected + 1) % pages.length
 	}
 </script>
 
-{#if $pageStore}
-	<section>
-		<button on:click={next} id="previous"><Icon>&gt;</Icon></button>
-		<div>
-			{#each [$pageStore[index]] as page (index)}
-				<Image src={page} alt="Page preview" />
-			{/each}
-		</div>
-		<button on:click={next} id="previous"><Icon>&lt;</Icon></button>
-	</section>
-{/if}
+<style>
+	:global(img) {
+		box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12),
+			0 3px 5px -1px rgba(0, 0, 0, 0.2);
+	}
+
+	section {
+		display: flex;
+		align-items: center;
+		height: 32rem;
+	}
+
+	button {
+		border: none;
+		background-color: #e0e0e0;
+		opacity: 0.8;
+		padding: 0.8rem;
+		width: 3rem;
+		height: 3rem;
+		cursor: pointer;
+		position: absolute;
+		z-index: 1;
+	}
+
+	#next {
+		transform: rotate(180deg);
+		right: 24px;
+	}
+
+	#previous {
+		left: 24px;
+	}
+
+	button:hover {
+		background-color: #bdbdbd;
+	}
+
+	div {
+		position: relative;
+		z-index: -1;
+		margin: var(--space-s);
+	}
+</style>
+
+<section>
+	<button on:click={next} id="previous"><Icon><AnchorIcon /></Icon></button>
+	<div>
+		{#each [pages[selected]] as page (selected)}
+			<Image src={page} alt="Page preview" />
+		{/each}
+	</div>
+	<button on:click={next} id="next"><Icon><AnchorIcon /></Icon></button>
+</section>
