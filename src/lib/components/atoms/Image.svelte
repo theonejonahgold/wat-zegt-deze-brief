9d4d580fd7b9b5b688bb2c16868989246ac5161c
benl95 <beno_1995@hotmail.com>
2021-05-28 09:20:23 +0200

    feat: add shadow prop to image

--- src/lib/components/atoms/Image.svelte
+++ src/lib/components/atoms/Image.svelte
@@ -1,13 +1,19 @@
 <script>
 	export let src: string
 	export let alt: string = ''
+	export let shadow: boolean = false
 </script>
 
 <style>
+	.shadow {
+		box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12),
+			0 3px 5px -1px rgba(0, 0, 0, 0.2);
+	}
+
 	img {
 		display: inline-block;
 		width: 100%;
 	}
 </style>
 
-<img {src} {alt} aria-hidden={!alt} />
+<img {src} {alt} aria-hidden={!alt} class={shadow ? 'shadow' : ''} />
