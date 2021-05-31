212ad494fce916db4a7c4334f14e39baaf346050
benl95 <beno_1995@hotmail.com>
2021-05-31 22:34:44 +0200

    feat: add record audio component

---
+++ src/lib/components/atoms/RecordAudio.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { browser } from '$app/env'
+
+	let recorder: MediaRecorder
+	let src: any
+	let chunks: any[] = []
+
+	if (browser) {
+		navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+			recorder = new MediaRecorder(stream)
+		})
+	}
+
+	function recordMedia() {
+		recorder.start()
+		console.log(recorder.state)
+
+		recorder.ondataavailable = (e: BlobEvent) => {
+			chunks.push(e.data)
+		}
+
+		recorder.onstop = () => {
+			let blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
+			chunks = []
+			console.log(blob)
+
+			if (window) {
+				const audioURL = window.URL.createObjectURL(blob)
+				src = audioURL
+			}
+		}
+	}
+
+	function stopMedia() {
+		recorder.stop()
+		console.log(recorder.state)
+	}
+</script>
+
+<button on:click={recordMedia}>Record</button>
+<button on:click={stopMedia}>Stop</button>
+
+<article>
+	<audio controls {src} />
+</article>
