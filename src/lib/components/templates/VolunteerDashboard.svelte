14d69277c7172b7f661c5430ed07becdafcedd72
bvictorb <victory-boucher@hotmail.com>
2021-05-31 13:09:10 +0200

    feat: split up user and volunteer dashboards into templates

---
+++ src/lib/components/templates/VolunteerDashboard.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { SpokenText } from '$atoms'
+	import { VolunteerLetterCard } from '$organisms'
+	import Header from './Header.svelte'
+	import type { Letter } from '$types'
+
+	export let letters: Letter[]
+</script>
+
+<style>
+	ul {
+		list-style: none;
+		padding: 0;
+		display: grid;
+		row-gap: var(--space-m);
+		margin-top: var(--space-xs);
+	}
+</style>
+
+<Header>
+	<SpokenText --align="center" slot="middle" text="Brieven" />
+	<p slot="right">filter</p>
+</Header>
+<main>
+	<section>
+		<ul>
+			{#each letters as letter (letter.id)}
+				<li>
+					<VolunteerLetterCard {letter} />
+				</li>
+			{/each}
+		</ul>
+	</section>
+</main>
