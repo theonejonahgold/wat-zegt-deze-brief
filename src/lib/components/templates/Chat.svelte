cf678d8a71718f41850a1348f0ff64b49e91f374
benl95 <beno_1995@hotmail.com>
2021-06-02 13:55:37 +0200

    feat: add chat template

---
+++ src/lib/components/templates/Chat.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Header from './Header.svelte'
+	import { SpokenText, Help, Back } from '$atoms'
+	import { RecordAudio } from '$molecules'
+	import { browser } from '$app/env'
+	import { checkRole } from '$db/user'
+	import type { Load } from '@sveltejs/kit'
+
+	let recorder: MediaRecorder
+
+	if (browser) {
+		navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+			recorder = new MediaRecorder(stream)
+		})
+	}
+</script>
+
+<style>
+	footer {
+		box-shadow: var(--bs-up);
+		padding-top: var(--space-m);
+		width: 100%;
+		overflow-x: scroll;
+	}
+</style>
+
+<Header>
+	<Back slot="left" href="/dashboard" />
+	<SpokenText --align="center" slot="middle" text="Gesproken bericht" />
+	<Help slot="right" />
+</Header>
+<main />
+<footer>
+	<SpokenText
+		--align="center"
+		text="Klik op de microfoon en stel nog een vraag of bedank de vrijwilliger"
+	/>
+	<!-- <RecordAudio {recorder} /> -->
+</footer>
