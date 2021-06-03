f9473f6df5b547739af3fb4145dc29d595eaa7ae
benl95 <beno_1995@hotmail.com>
2021-06-03 16:57:29 +0200

    refactor: save data in supabase on voice message

--- src/lib/actions/message.ts
+++ src/lib/actions/message.ts
@@ -1,16 +1,25 @@
-import { v4 as uuid } from 'uuid'
 import { client } from '$config/supabase'
 import { browser } from '$app/env'
+import { definitions } from '$types'
 
-export async function messageHandler(e: any) {
-	const message: File = e.detail
-	if (!message) return
+export async function messageHandler({ detail: message }: CustomEvent<File>) {
+	if (!message || !browser) return
 
-	if (browser) {
-		const storage = localStorage.getItem('supabase.auth.token')
-		const currentSession = JSON.parse(storage)
-		const { id } = currentSession.currentSession.user
-
-		await client.storage.from('messages').upload(`${id}`, message)
-	}
+	const splitPathname = window.location.pathname.split('/')
+	const id = splitPathname[splitPathname.length - 1]
+	const userId = client.auth.session().user.id
+	const currentDate = new Date().toLocaleString()
+	await client.storage.from('messages').upload(`${id}/${userId}/${currentDate}`, message)
+	const { body } = await client
+		.from<definitions['messages']>('messages')
+		.insert({
+			sender_id: userId,
+			content: `${id}/${userId}/${currentDate}`,
+			type: 'audio',
+		})
+		.single()
+	await client.rpc('update_letter_messages', {
+		letter_id: id,
+		message_id: body.id,
+	})
 }
