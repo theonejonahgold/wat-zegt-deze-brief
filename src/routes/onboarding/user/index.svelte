a750b9eb98f373b78c9f30f49fd475ee2877f447
bvictorb <victory-boucher@hotmail.com>
2021-06-03 15:04:54 +0200

    feat: add page to let user know they will receive instructions

---
+++ src/routes/onboarding/user/index.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { SpokenText, Back, Button, Image, Help } from '$atoms'
+	import { Header, Flex } from '$templates'
+</script>
+
+<style>
+	div {
+		width: 100%;
+	}
+	.image-container {
+		padding: var(--space-xxl);
+		background: var(--secondary);
+		border-radius: var(--border-radius);
+		display: flex;
+		justify-content: center;
+
+		:global(img) {
+			width: 100%;
+		}
+	}
+</style>
+
+<Header>
+	<Back slot="left" href="/onboarding" />
+	<SpokenText --align="center" slot="middle" text="We laten je eerst zien hoe deze app werkt" />
+	<Help slot="right" />
+</Header>
+
+<Flex>
+	<div class="image-container">
+		<Image src="/images/onboarding-user-5.png" alt="Instructions" />
+	</div>
+	<div>
+		<Button href="/onboarding/user?step=1">Volgende</Button>
+	</div>
+</Flex>
