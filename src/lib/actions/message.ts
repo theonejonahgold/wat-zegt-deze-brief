04f0794174e5c0d6f2ccf30be5fc2ccf44e74e16
benl95 <beno_1995@hotmail.com>
2021-06-03 15:25:20 +0200

    refactor: upload message with uuid

--- src/lib/actions/message.ts
+++ src/lib/actions/message.ts
@@ -1,10 +1,16 @@
 import { v4 as uuid } from 'uuid'
 import { client } from '$config/supabase'
+import { browser } from '$app/env'
 
 export async function messageHandler(e: any) {
 	const message: File = e.detail
 	if (!message) return
 
-	const id = uuid()
-	await client.storage.from('messages').upload(`${id}`, message)
+	if (browser) {
+		const storage = localStorage.getItem('supabase.auth.token')
+		const currentSession = JSON.parse(storage)
+		const { id } = currentSession.currentSession.user
+
+		await client.storage.from('messages').upload(`${id}`, message)
+	}
 }
