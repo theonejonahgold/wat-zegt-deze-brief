a76c83c3e0b4e689c9bc324b2701c22ea68c1eeb
Jonah Meijers <jonahmeijers97@gmail.com>
2021-06-07 13:10:48 +0200

    feat: create loader component

---
+++ src/lib/components/atoms/Loader.svelte
@@ -0,0 +1,37 @@
+<style>
+	div {
+		width: var(--space-xl);
+		height: var(--space-xl);
+		aspect-ratio: 1;
+		display: grid;
+		border-radius: 50%;
+		-webkit-mask: radial-gradient(farthest-side, #0000 40%, #000 41%);
+		background: linear-gradient(0deg, rgb(0 0 0/50%) 50%, rgb(0 0 0/100%) 0) center/2px 100%,
+			linear-gradient(90deg, rgb(0 0 0/25%) 50%, rgb(0 0 0/75%) 0) center/100% 2px;
+		background-repeat: no-repeat;
+		animation: s3 1s infinite steps(12);
+	}
+
+	div::before,
+	div::after {
+		content: '';
+		grid-area: 1/1;
+		border-radius: 50%;
+		background: inherit;
+		opacity: 0.915;
+		transform: rotate(30deg);
+	}
+
+	div::after {
+		opacity: 0.83;
+		transform: rotate(60deg);
+	}
+
+	@keyframes s3 {
+		100% {
+			transform: rotate(1turn);
+		}
+	}
+</style>
+
+<div />
