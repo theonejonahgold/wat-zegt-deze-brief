2c5867ec8976ad29cc4dca1c235daf3c6474d51b
Jonah Meijers <jonahmeijers97@gmail.com>
2021-06-08 15:23:08 +0200

    feat: save letter onboarding state in db

---
+++ src/routes/api/onboarding/letter.ts
@@ -0,0 +1,16 @@
+import { client } from '$config/supabase'
+import type { RequestHandler } from '@sveltejs/kit'
+
+export const get: RequestHandler = async ({ query }) => {
+	const { error } = await client.from('onboardings').update({
+		letter_onboarding: true,
+	})
+	if (error) throw error
+
+	return {
+		status: 303,
+		headers: {
+			location: query.get('redirect') || '/api/letter',
+		},
+	}
+}
