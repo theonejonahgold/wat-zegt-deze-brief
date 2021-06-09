80758bc5a42a40d5b060df2a47004e79dc56ee35
benl95 <beno_1995@hotmail.com>
2021-06-09 16:58:55 +0200

    refactor: remove date from path use uuid instead

--- src/lib/db/messageHandler.ts
+++ src/lib/db/messageHandler.ts
@@ -1,6 +1,7 @@
 import { client } from '$config/supabase'
 import { browser } from '$app/env'
 import type { definitions } from '$types'
+import { v4 as uuid } from 'uuid'
 
 export async function messageHandler({ detail: message }: CustomEvent<File>) {
 	if (!message || !browser) return
@@ -8,18 +9,18 @@ export async function messageHandler({ detail: message }: CustomEvent<File>) {
 	const splitPathname = window.location.pathname.split('/')
 	const id = splitPathname[splitPathname.length - 1]
 	const userId = client.auth.session().user.id
-	const currentDate = new Date().toLocaleString()
-	await client.storage.from('messages').upload(`${id}/${userId}/${currentDate}`, message)
+	const messageId = uuid()
+	await client.storage.from('messages').upload(`${id}/${userId}/${messageId}`, message)
 	const { body } = await client
 		.from<definitions['messages']>('messages')
 		.insert({
 			sender_id: userId,
-			content: `${id}/${userId}/${currentDate}`,
+			content: `${id}/${userId}/${messageId}`,
 			type: 'audio',
 		})
 		.single()
 	await client.rpc('update_letter_messages', {
 		letter_id: id,
-		message_id: body.id,
+		message_id: messageId,
 	})
 }
