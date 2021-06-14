0d7501857c2ee9c3041ebaa65dd00717429118f0
bvictorb <victory-boucher@hotmail.com>
2021-06-14 10:21:09 +0200

    feat: add filter component

---
+++ src/lib/components/organisms/Filter.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { quintInOut } from 'svelte/easing'
+	import { fly } from 'svelte/transition'
+
+	export let show = false
+</script>
+
+<style>
+	div {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: hotpink;
+	}
+</style>
+
+{#if show}
+	<div transition:fly={{ y: 200, duration: 500, easing: quintInOut }}>yo</div>
+{/if}
